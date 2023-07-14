c1d44cb66c4a64071f92c2d5aaa893b67fe294a9
Menna Elbakry <45173110+Menna-Elbakry@users.noreply.github.com>
2023-07-15 01:14:50 +0300

    updateProduct.go

--- api/updateProduct.go
+++ api/updateProduct.go
@@ -22,13 +22,9 @@ func UpdateProduct(c *gin.Context) {
 	id := c.Param("id")
 
 	var product model.Product
-	err = c.ShouldBindJSON(&product)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
-		return
-	}
+
 	//Update Query
-	_, err = db.Exec("UPDATE products SET name=$1,quantity=$2, price=$3 WHERE id=$4", product.ProductName, product.Quantity, product.Price, id)
+	_, err = db.Exec(`UPDATE public."product" SET product_name=$1,quantity=$2,price=$3 WHERE product_id=$4;`, product.ProductName, product.Quantity, product.Price, id)
 	if err != nil {
 		log.Println(err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update product"})
